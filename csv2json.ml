aa78b96498a2cb0761ba6968559f75db42925893
Martin Jambon <martin.jambon@mrnumber.com>
2013-03-22 14:45:57 -0700

    Support for numbered fields (-n, -n0, n1)

--- csv2json.ml
+++ csv2json.ml
@@ -1,6 +1,17 @@
 open Printf
 
+type header_kind = First_line | Numbered of int
+
+let make_numbered_labels first l =
+  let _, acc =
+    List.fold_left
+      (fun (i, acc) x -> (i+1, (string_of_int i, x) :: acc))
+      (first, []) l
+  in
+  List.rev acc
+
 let main () =
+  let header_kind = ref First_line in
   let delim = ref ',' in
   let options = [
     "-d", Arg.String (
@@ -13,6 +24,20 @@ let main () =
     ),
     "<delimiter>
           Field delimiter (single byte or the string 'TAB'; default: ',')";
+
+    "-n", Arg.Unit (fun () -> header_kind := Numbered 1),
+    "
+          Use numbers for field labels, treat first row as data";
+
+    "-n0", Arg.Unit (fun () -> header_kind := Numbered 0),
+    "
+          Use numbers for field labels starting from 0,
+          treat first row as data";
+
+    "-n1", Arg.Unit (fun () -> header_kind := Numbered 0),
+    "
+          Use numbers for field labels starting from 1,
+          treat first row as data (same as -n)";
   ]
   in
   let anon_fun s = raise (Arg.Bad ("Don't know what to do with " ^ s)) in
@@ -22,14 +47,23 @@ let main () =
   let oc = stdout in
   let stream = Csv.of_channel ~separator: !delim ic in
   let errors = ref 0 in
+  let header_kind = !header_kind in
   try
-    let head = Csv.next stream in
+    let head =
+      match header_kind with
+          First_line -> Csv.next stream
+        | Numbered first -> []
+    in
     while true do
       try
         let pairs =
-          let x = Csv.next stream in
-          try List.combine head x
-          with _ -> incr errors; raise Exit
+          let row = Csv.next stream in
+          match header_kind with
+              First_line ->
+                (try List.combine head row
+                 with _ -> incr errors; raise Exit)
+            | Numbered first ->
+                make_numbered_labels first row
         in
         let fields =
           List.fold_right (
